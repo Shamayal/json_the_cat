1bdb668173d07c68ec59b480a406ad8be148638e
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-16 01:10:33 -0400

    test conditions and eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,34 @@
 const request = require('request'); // to use request library and make the HTTP request
 
-let breedName  = process.argv[2];
+// const fetchBreedDescription = function(breedName, callback) {
+//   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+//     if (error) {
+//       console.log("Error: domain name not found.")
+//     } else {
+//       try {
+//         const data = JSON.parse(body);
+//         console.log(data[0].description);
+//       } catch (error) {
+//         console.log("Sorry, we could not find that breed name in our data base.");
+//       }
+//     }
+//   });
+// };
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log("Error: domain name not found.")
-  } else {
-    try {
-      const data = JSON.parse(body);
-      console.log(data[0].description);
-    } catch (error) {
-      console.log("Sorry, we could not find that breed name in our data base.");
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
+    } else {
+      try {
+        const data = JSON.parse(body);
+        callback(null, data[0].description);
+      } catch (error) {
+        callback(error, "Sorry, we could not find that breed name in our database.");
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
